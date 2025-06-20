27d116e1f023883970bb7875a5aabb20057c450f
kaka1409 <khanhphqgcs230461@fpt.edu.vn>
2025-06-20 17:59:28 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <h1 class="text-3xl">
+    Mobile app
+  </h1>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <h1>
+    Mobile Header
+  </h1>
+</template>
